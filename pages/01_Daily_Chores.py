c21a30361cf424a46aa3aadb817b7926fe07b127
jakereckin <111083093+jakereckin@users.noreply.github.com>
2025-09-22 15:09:06 -0500

    Updates

--- pages/01_Daily_Chores.py
+++ pages/01_Daily_Chores.py
@@ -4,15 +4,17 @@ import pandas as pd
 
 
 # Define chores, durations, and days needed
-chores_enter = st.text_area(
+chores_enter = st.text_input(
     "Enter Chore Needed"
 )
-durations_enter = st.text_area(
-    "Enter Chore Duration (minutes)"
-)
-days_needed_enter = st.text_area(
-    "Enter Days Needed per Week"
-)
+if chores_enter:
+    durations_enter = st.number_input(
+        "Enter Chore Duration (minutes)", step=1
+    )
+    if durations_enter:
+        days_needed_enter = st.number_input(
+            "Enter Days Needed per Week", step=1, min_value=1, max_value=7
+        )
 add = st.button("Add Chores")
 reset = st.button("Reset Chores")
 if add:
@@ -20,6 +22,13 @@ if add:
     st.session_state.durations[chores_enter] = int(durations_enter)
     st.session_state.days_needed[chores_enter] = int(days_needed_enter)
     st.success(f"Added chore: {chores_enter}")
+    chore_frame = pd.DataFrame({
+        "Chore": st.session_state.chores,
+        "Duration (minutes)": [st.session_state.durations[c] for c in st.session_state.chores],
+        "Days Needed": [st.session_state.days_needed[c] for c in st.session_state.chores]
+    })
+    st.write("### Current Chores")
+    st.dataframe(chore_frame)
 
 max_minutes_per_day = st.number_input(
     "Max Minutes Per Day", min_value=1, value=120
